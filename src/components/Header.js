42df9eb6aa7767950053784d437a4954da53f6de
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-05-22 23:22:19 +0530

    Restyle Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,31 +3,36 @@ import samsung from "../images/Logos/samsung.png";
 import google from "../images/Logos/google.png";
 import oneplus from "../images/Logos/oneplus.png";
 import iqoo from "../images/Logos/iqoo.png";
+import menu from "../images/Logos/menu.svg";
 
 const Header = () => {
+  const logo = menu;
+
   return (
     <div className="header">
-      <div>Shopping Cart</div>
-      <ul className="brands-list">
-        <li>
-          <img src={samsung} alt="samsung" className="samsung-logo" />
-        </li>
-        <li>
-          <img src={apple} alt="apple" className="apple-logo" />
-        </li>
-        <li>
-          <img src={oneplus} alt="oneplus" className="oneplus-logo" />
-        </li>
-        <li>
-          <img src={google} alt="google" className="google-logo" />
-        </li>
-        <li>
-          <img src={iqoo} alt="iqoo" className="iqoo-logo" />
-        </li>
-      </ul>
+      <img src={logo} alt="logo" className="selected" />
+      <div className="main-logo">SmartPhonez</div>
       <div>View Cart</div>
     </div>
   );
 };
 
 export default Header;
+
+/* <ul className="brands-list">
+<li>
+  <img src={samsung} alt="samsung" className="samsung-logo" />
+</li>
+<li>
+  <img src={apple} alt="apple" className="apple-logo" />
+</li>
+<li>
+  <img src={oneplus} alt="oneplus" className="oneplus-logo" />
+</li>
+<li>
+  <img src={google} alt="google" className="google-logo" />
+</li>
+<li>
+  <img src={iqoo} alt="iqoo" className="iqoo-logo" />
+</li>
+</ul> */
